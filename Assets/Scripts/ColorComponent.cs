4d58ec216f651e0878c3ab132334d60a7d68faaf
Tobias Rogalski <dante.rogalski@gmail.com>
2017-01-21 01:26:00 +0100

    added ColorModel and ColorComponent

---
+++ Assets/Scripts/ColorComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class ColorComponent : MonoBehaviour {
+
+	ColorModelScript colorModel;
+
+	public ColorModelScript.Color ownColor;
+
+	// Use this for initialization
+	void Start () {
+		colorModel = ColorModelScript.instance;
+
+		colorModel.activeColor
+			/*.Where(x => ownColor == x)*/
+			.Subscribe (x => OnColorChanged(x)); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnColorChanged(ColorModelScript.Color newColor){
+		if (newColor == ownColor) {
+			//show
+		} else {
+			//hide
+		}
+	}
+}
---
+++ Assets/Scripts/ColorModelScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class ColorModelScript : MonoBehaviour {
+
+	public enum Color
+	{
+		PURPLE, BLUE, GREEN, YELLOW, ORANGE, RED
+	}
+		
+	public static ColorModelScript instance;
+
+	public ReactiveProperty<Color> activeColor;
+
+	public void setFrequence (float newFrequence) {
+		if (newFrequence < 10f)
+			activeColor = Color.PURPLE;
+		else if (newFrequence < 20f)
+			activeColor = Color.BLUE;
+		else if (newFrequence < 30f)
+			activeColor = Color.GREEN;
+		else if (newFrequence < 40f)
+			activeColor = Color.YELLOW;
+		else if (newFrequence < 50f)
+			activeColor = Color.ORANGE;
+		else
+			activeColor = Color.RED;
+	}
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		activeColor = new ReactiveProperty<Color> ();
+
+		activeColor
+			.Where(activeColor => ownColor == activeColor)
+			.Subscribe (activeColor => OnColorChanged); 
+	}
+
+	OnColorChanged(Color)
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
