81a4c07b44be83ba0b5c9900b39f147c237f248a
Sushiy <julivizi@yahoo.de>
2017-01-21 18:09:46 +0100

    handle midi input

--- Assets/Scripts/MidiController.cs
+++ Assets/Scripts/MidiController.cs
@@ -3,25 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using MidiJack;
 
+
+
 public class MidiController : MonoBehaviour
 {
+    private Dictionary<int, int> IndexToPiano;
+
+    private Dictionary<int, int> PianoToIndex;
+
+    private const int NUMOFKEYS = 8;
+    private int buttonOffset = 48;
+    public GameObject audioParent;
+    AudioSource[] audioSources;
+
     public bool[] buttonPressed;
+    private int pressedButtonCount = 0;
 
     private void Start()
     {
-        buttonPressed = new bool[30];
+        if (audioParent == null)
+        {
+            Debug.LogError("MidiController is lacking references");
+            return;
+        }
+        audioSources = new AudioSource[NUMOFKEYS];
+        IndexToPiano = new Dictionary<int, int>() { { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 5 }, { 4, 7 }, { 5, 9 }, { 6, 11 }, { 7, 12 } };
+        PianoToIndex = new Dictionary<int, int>() { { 0, 0 }, { 2, 1 }, { 4, 2 }, { 5, 3 }, { 7, 4 }, { 9, 5 }, { 11, 6 }, { 12, 7 } };
+        for (int i = 0; i < NUMOFKEYS; i++)
+        {
+            audioSources[i] = audioParent.transform.GetChild(i).GetComponent<AudioSource>();
+        }
+
+        buttonPressed = new bool[NUMOFKEYS];
     }
 
     private void Update()
     {
-        if (MidiMaster.GetKeyDown(0))
+        for (int i = 0 ; i < NUMOFKEYS; i++)
         {
-            buttonPressed[0] = true;
-        }
-        /*
-        for (int i = 0 ; i < 19; i++)
-        {
-            if(MidiMaster.GetKey(i + 30) > 0)
+            if(MidiMaster.GetKey(GetIndexToPiano(i) + buttonOffset) > 0)
             {
                 buttonPressed[i] = true;
             }
@@ -29,22 +49,28 @@ public class MidiController : MonoBehaviour
             {
                 buttonPressed[i] = false;
             }
-        }*/
+        }
     }
 
     void NoteOn(MidiChannel channel, int note, float velocity)
     {
-        Debug.Log("NoteOn: " + channel + "," + note + "," + velocity);
+        audioSources[GetPianoToIndex(note - buttonOffset)].pitch = Mathf.Pow(1.0594631f, note - (buttonOffset + NUMOFKEYS/2));
+        audioSources[GetPianoToIndex(note - buttonOffset)].Play();
+        pressedButtonCount++;
+        //Debug.Log("NoteOn: " + channel + "," + note + "," + velocity);
     }
 
     void NoteOff(MidiChannel channel, int note)
     {
-       Debug.Log("NoteOff: " + channel + "," + note);
+        pressedButtonCount = (pressedButtonCount - 1) < 0? 0:pressedButtonCount -1;
+        audioSources[GetPianoToIndex(note - buttonOffset)].Stop();
+
+        //Debug.Log("NoteOff: " + channel + "," + note);
     }
 
     void Knob(MidiChannel channel, int knobNumber, float knobValue)
     {
-        Debug.Log("Knob: " + knobNumber + "," + knobValue);
+        //Debug.Log("Knob: " + knobNumber + "," + knobValue);
     }
 
     void OnEnable()
@@ -59,5 +85,19 @@ public class MidiController : MonoBehaviour
         MidiMaster.noteOnDelegate -= NoteOn;
         MidiMaster.noteOffDelegate -= NoteOff;
         MidiMaster.knobDelegate -= Knob;
+    }
+
+    int GetPianoToIndex(int i)
+    {
+        if (PianoToIndex.ContainsKey(i))
+            return PianoToIndex[i];
+        return 0;
+    }
+
+    int GetIndexToPiano(int i)
+    {
+        if (IndexToPiano.ContainsKey(i))
+            return IndexToPiano[i];
+        return 0;
     }
 }
